6a56ba1bed989b4164c16d4367a94c4ad059e7f4
David Wahl <dmwahl@eagles.usi.edu>
2020-06-27 11:50:01 -0500

    Third time is the charm

--- Test.R
+++ Test.R
@@ -1,3 +1,7 @@
 print("this file was created within RStudio")
 
 print("and now it lives on GitHub")
+
+print("I want to try one more time to see if this link is working")
+
+print("test one more time")
